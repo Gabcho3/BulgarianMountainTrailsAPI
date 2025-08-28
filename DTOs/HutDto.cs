3174e4274c32a99c8e359bd2b2b61540d8b358bb
Gabriel Dimitrov <gabriel_work@abv.bg>
2025-08-28 16:02:29 +0300

    Add DTOs for Hut and Trail entities

---
+++ DTOs/HutDto.cs
@@ -0,0 +1,28 @@
+﻿namespace BulgarianMountainTrailsAPI.DTOs
+{
+    public class HutDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Mountain { get; set; }
+        public int Altitude { get; set; }
+        public int Capacity { get; set; }
+        public string? PhoneNumber { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public List<SimpleTrailDto> Trails { get; set; } = new();
+    }
+
+    public class SimpleTrailDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Mountain { get; set; }
+        public string Difficulty { get; set; } = null!;
+        public double LengthKm { get; set; }
+        public double DurationHours { get; set; }
+        public string StartPoint { get; set; } = null!;
+        public string EndPoint { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
---
+++ DTOs/TrailDto.cs
@@ -0,0 +1,37 @@
+﻿namespace BulgarianMountainTrailsAPI.DTOs
+{
+    public class TrailDto
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Mountain { get; set; }
+
+        public string Difficulty { get; set; } = null!;
+
+        public double LengthKm { get; set; }
+
+        public double DurationHours { get; set; }
+
+        public string StartPoint { get; set; } = null!;
+
+        public string EndPoint { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public List<SimpleHutDto> Huts { get; set; } = new();
+    }
+
+    public class SimpleHutDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Mountain { get; set; }
+        public int Altitude { get; set; }
+        public int Capacity { get; set; }
+        public string? PhoneNumber { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
